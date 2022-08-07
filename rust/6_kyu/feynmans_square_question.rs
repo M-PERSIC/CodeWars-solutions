a776c914865dec4e679fa7e95c79b65bbc11429d
M-PERSIC <michael.a.persico@gmail.com>
2022-08-07 01:51:47 -0400

    New Rust solution

---
+++ rust/6_kyu/feynmans_square_question.rs
@@ -0,0 +1,19 @@
+/*
+Michael Persico
+Aug.07, 2022
+Feynman's square question
+https://www.codewars.com/kata/551186edce486caa61000f5c
+*/
+
+fn count_squares (n: u32) -> usize {
+    (0..n).rev().map(|x| (n as u64 - x as u64).pow(2)).sum::<u64>() as usize
+}
+
+fn main() {
+    println!("{}", count_squares(1)); // 1
+    println!("{}", count_squares(2)); // 5
+    println!("{}", count_squares(3)); // 14
+    println!("{}", count_squares(5)); // 55
+    println!("{}", count_squares(8)); // 204
+    println!("{}", count_squares(15)); // 1240
+}
