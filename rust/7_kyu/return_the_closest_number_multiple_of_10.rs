6ba4063ad157ce342cacdefaeb809af890355f52
M-PERSIC <michael.a.persico@gmail.com>
2022-08-10 16:57:46 -0400

    New Rust solution

---
+++ rust/7_kyu/return_the_closest_number_multiple_of_10.rs
@@ -0,0 +1,20 @@
+/*
+Michael Persico
+Aug.10, 2022
+Return the closest number multiple of 10
+https://www.codewars.com/kata/58249d08b81f70a2fc0001a4
+*/
+
+fn closest_multiple_of_10(n: u32) -> u32 {
+        let last_digit: u32 = n.to_string().chars().last().unwrap().to_digit(10).unwrap();
+        match last_digit {
+            last_digit if last_digit >= 5 => n + 10 - last_digit,
+            _ => n - last_digit
+    }
+}
+
+fn main() {
+    println!("{}", closest_multiple_of_10(54)); // 50
+    println!("{}", closest_multiple_of_10(55)); // 60
+}
+
