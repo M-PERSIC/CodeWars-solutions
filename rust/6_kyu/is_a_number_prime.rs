9f00ee9a58745a5eb957cc627e27c36590927937
M-PERSIC <michael.a.persico@gmail.com>
2022-08-18 00:21:02 -0400

    New Rust solution

---
+++ rust/6_kyu/is_a_number_prime.rs
@@ -0,0 +1,29 @@
+/*
+Michael Persico
+Aug. 18, 2022
+Is a number prime?
+https://www.codewars.com/kata/5262119038c0985a5b00029f
+*/
+
+fn is_prime(x: i64) -> bool {
+    match x {
+        x if x <= 0 || x == 1 => false,
+        _ => (2..(x as f64).sqrt() as i64 + 1).all(|num| x % num != 0)
+    }    
+}
+
+fn main() {
+    println!("{}", is_prime(4)); // false
+    println!("{}", is_prime(6)); // false
+    println!("{}", is_prime(8)); // false
+    println!("{}", is_prime(9)); // false
+    println!("{}", is_prime(45)); // false
+    println!("{}", is_prime(-5)); // false
+    println!("{}", is_prime(-8)); // false
+    println!("{}", is_prime(-41)); // false
+    println!("{}", is_prime(0)); // false
+    println!("{}", is_prime(3)); // true
+    println!("{}", is_prime(73)); // true
+    println!("{}", is_prime(4)); // false
+    println!("{}", is_prime(2)); // true
+}
