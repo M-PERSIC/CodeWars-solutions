3a848d63958c3759c40e1b857e59ebb122d51a12
M-PERSIC <michael.a.persico@gmail.com>
2022-08-22 00:05:54 -0400

    New Rust solution

---
+++ rust/6_kyu/round_by_0,5_steps.rs
@@ -0,0 +1,21 @@
+/*
+Michael Persico
+Aug.22, 2022
+Round by 0.5 steps
+https://www.codewars.com/kata/51f1342c76b586046800002a
+*/
+
+fn solution(n: f64) -> f64 {
+    match n {
+        n if n % 1.0 < 0.25 => f64::floor(n),
+        n if n % 1.0 < 0.5 || n % 1.0 < 0.75 => f64::floor(n) + 0.5,
+        _ => f64::ceil(n)
+    }
+}
+
+fn main() {
+    println!("{:.1}", solution(4.2)); // 4.0
+    println!("{:.1}", solution(4.4)); // 4.5
+    println!("{:.1}", solution(4.6)); // 4.5
+    println!("{:.1}", solution(4.8)); // 5.0
+}
