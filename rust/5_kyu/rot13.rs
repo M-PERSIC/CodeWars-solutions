d7fdc9c0bddd0b6de1a92cec193fa42b2624fa40
M-PERSIC <michael.a.persico@gmail.com>
2022-08-25 20:44:40 -0400

    Code refactoring

--- rust/5_kyu/rot13.rs
+++ rust/5_kyu/rot13.rs
@@ -8,8 +8,8 @@ https://www.codewars.com/kata/530e15517bc88ac656000716
 fn rot13(message: &str) -> String {
     message.chars()
     .map(|x| match x {
-        x if x.is_alphabetic() && x.is_uppercase() => char::from_u32(((x as u32 - 52) % 26) + 65).unwrap(),
-        x if x.is_alphabetic() && x.is_lowercase() => char::from_u32(((x as u32 - 84) % 26) + 97).unwrap(),
+        x if x.is_uppercase() => char::from_u32(((x as u32 - 52) % 26) + 65).unwrap(),
+        x if x.is_lowercase() => char::from_u32(((x as u32 - 84) % 26) + 97).unwrap(),
         _ => x,
     })
     .collect()
